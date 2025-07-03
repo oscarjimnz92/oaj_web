fd507bbb145afd444bdc68c7de9edcb3a6e3f439
Oscar A <ozcar.21@hotmail.com>
2025-07-03 00:16:12 -0600

    Proyecto OAJWEB

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
+
 # oaj_web
 OAJ Data Intelligence's Web
+
+---
+title: oaj-web-design-1
+emoji: 🐳
+colorFrom: yellow
+colorTo: pink
+sdk: static
+pinned: false
+tags:
+  - deepsite
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ app.py
@@ -0,0 +1,59 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+
+load_dotenv()  # Cargar variables desde .env
+
+app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
+
+# Configuración de correo usando variables del entorno
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+
+mail = Mail(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html', active_page='index')
+
+
+@app.route('/mensaje', methods=['POST'])
+def mensaje():
+    nombre = request.form.get('name')
+    email = request.form.get('email')
+    service = request.form.get('service')
+    mensajes = request.form.get('message')
+
+    msg = Message('Nuevo mensaje desde OAJ Web',
+                  sender=app.config['MAIL_USERNAME'],
+                  recipients=['ozcar.21@hotmail.com'])
+    msg.body = f"Nombre: {nombre}\nEmail: {email}\nServicio: {service}\n\nMensaje:\n{mensajes}"
+    mail.send(msg)
+
+    flash('¡Mensaje enviado correctamente!')
+    return redirect(request.referrer or url_for('index'))
+
+@app.route('/acerca')
+def acerca():
+    return render_template('acerca.html', active_page='acerca')
+
+
+@app.route('/servicios')
+def servicios():
+    return render_template('servicios.html', active_page='servicios')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html', active_page='contacto')
+
+@app.route('/privacidad')
+def privacidad():
+    return render_template('privacidad.html', active_page='privacidad')
+
+if __name__ == '__main__':
+    app.run(debug=True)
